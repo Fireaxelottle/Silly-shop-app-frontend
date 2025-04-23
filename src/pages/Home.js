e265b42b8afe9951a8fe2d09311e07e783c50f7c
Fireaxelottle <asgharnaseem1975@gmail.com>
2025-04-23 20:17:32 +0500

    Pagination

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,60 +1,102 @@
-import React, {useEffect,useState} from 'react'
-import '../css/Home.css'
-import Card from '../components/Card'
-import axios from 'axios'
-
-
-
-
-
+import React, { useEffect, useState } from "react";
+import "../css/Home.css";
+import Card from "../components/Card";
+import axios from "axios";
 
 function Home() {
-
-  const [query] = useState('');
-
-  const [data,setData] = useState([
-    { _id: "67a4ee06f8c4745a0fbf0fbc", name: "Adidas Shoes", photo: "uploads/b6d62f0a-9f0e-477c-bb45-7aa4192bbace.jpg", price: 1000,}, ,
-     { _id: "67a4ee06f8c4745a0fbf0fbc", name: "Adidas Shoes", photo: "uploads/b6d62f0a-9f0e-477c-bb45-7aa4192bbace.jpg", price: 1000,},
-     { _id: "67a4ee06f8c4745a0fbf0fbc", name: "Adidas Shoes", photo: "uploads/b6d62f0a-9f0e-477c-bb45-7aa4192bbace.jpg", price: 1000,},
-     { _id: "67a4ee06f8c4745a0fbf0fbc", name: "Adidas Shoes", photo: "uploads/b6d62f0a-9f0e-477c-bb45-7aa4192bbace.jpg", price: 1000,}
-    ]);
-
-   const Getdata = async()=>{
-      try {
-      const response = await axios.get('http://localhost:5000/api/v1/product/latest');
-      setData(response.data.products);
   
+  const [query] = useState("");
+  const [page, setPage] = useState(1);
+
+  const [data, setData] = useState([
+    {
+      _id: "67a4ee06f8c4745a0fbf0fbc",
+      name: "Adidas Shoes",
+      photo: "uploads/b6d62f0a-9f0e-477c-bb45-7aa4192bbace.jpg",
+      price: 1000,
+    },
+    {
+      _id: "67a4ee06f8c4745a0fbf0fbc",
+      name: "Adidas Shoes",
+      photo: "uploads/b6d62f0a-9f0e-477c-bb45-7aa4192bbace.jpg",
+      price: 1000,
+    },
+    {
+      _id: "67a4ee06f8c4745a0fbf0fbc",
+      name: "Adidas Shoes",
+      photo: "uploads/b6d62f0a-9f0e-477c-bb45-7aa4192bbace.jpg",
+      price: 1000,
+    },
+    {
+      _id: "67a4ee06f8c4745a0fbf0fbc",
+      name: "Adidas Shoes",
+      photo: "uploads/b6d62f0a-9f0e-477c-bb45-7aa4192bbace.jpg",
+      price: 1000,
+    },
+  ]);
+
+  const Getdata = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/v1/product/latest"
+      );
+      setData(response.data.products);
     } catch (error) {
-      console.error('Error fetching search results:', error);
-    };
-    };
+      console.error("Error fetching search results:", error);
+    }
+  };
+
+  const isPrevPage = page > 1;
+  const isNextPage = page < data.length / 10;
+
+  const itemsPerPage = 10;
+  const paginatedItems = data.slice(
+    (page - 1) * itemsPerPage,
+    page * itemsPerPage
+  );
+
+ 
 
   useEffect(() => {
     Getdata();
-  },[query]);
+  }, [query]);
   console.log(data);
   return (
     <div className="home">
-        <div className='Showcase'>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRkkE4MAAkihr1xFfnmeMZPRNH4JEhIA-QZA&s' alt="" />
-            <div className="cover">
-            </div>
-            <div className='data'>
-                <h1>EVERYTHING.</h1>
-                <p>You Buy. We Sell.</p>
-            </div>      
+      <div className="Showcase">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRkkE4MAAkihr1xFfnmeMZPRNH4JEhIA-QZA&s"
+          alt=""
+        />
+        <div className="cover"></div>
+        <div className="data">
+          <h1>EVERYTHING.</h1>
+          <p>You Buy. We Sell.</p>
         </div>
-        <div className="main">
-
-          <h1>Shop</h1>
-          <div className='cards'>
-            {data.map((item) => (
-              <Card key={item._id} item={item}/>
-            ))}
-          </div>
+      </div>
+      <div className="main">
+        <h1>Shop</h1>
+        <div className="cards">
+          {paginatedItems.map((item) => (
+            <Card key={item._id} item={item} />
+          ))}
         </div>
-    </div>  
-  )
+        <div className="pgch">
+          <button
+            disabled={!isPrevPage}
+            onClick={() => setPage((prev) => prev - 1)}>
+            Prev
+          </button>
+          <span>{page}</span>
+          <button
+            disabled={!isNextPage}
+            onClick={() => setPage((prev) => prev + 1)}>
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -12,7 +12,20 @@ function Search() {
   const [category, setCategory] = useState("ALL");
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState("");
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState([
+    {
+      _id: "67a4ee06f8c4745a0fbf0fbc",
+      name: "Adidas Shoes",
+      photo: "uploads/b6d62f0a-9f0e-477c-bb45-7aa4192bbace.jpg",
+      price: 1000,
+    },
+    {
+      _id: "67a4ee06f8c4745a0fbf0fbc",
+      name: "Adidas Shoes",
+      photo: "uploads/b6d62f0a-9f0e-477c-bb45-7aa4192bbace.jpg",
+      price: 1000,
+    }
+  ]);
 
   const newData = async () => {
     try {
@@ -31,32 +44,50 @@ function Search() {
   }, [query]);
 
   const isPrevPage = page > 1;
-  const isNextPage = page < 4;
+  const isNextPage = page < results.length / 10;
+
+  const itemsPerPage = 10;
+  const paginatedItems = results.slice(
+    (page - 1) * itemsPerPage,
+    page * itemsPerPage
+  );
 
   return (
     <div className="container">
       <h1>Search</h1>
-        {screenWidth > 1000 ? (
-          ""
-        ) : (
-          <div className="ser">
-            <i className="fa-solid fa-magnifying-glass"></i>
-            <input
-              type="text"
-              value={query}
-              onChange={(e) => {
-                setQuery(e.target.value);
-                newData();
-              }}
-              placeholder="Search by name..."
-            />
-          </div>
-        )}
+      {screenWidth > 1000 ? (
+        ""
+      ) : (
+        <div className="ser">
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+              newData();
+            }}
+            placeholder="Search by name..."
+          />
+        </div>
+      )}
       <div className="search">
-        
-
         <div className="choises">
           {screenWidth > 1000 ? <h1>Filter</h1> : ""}
+
+          <div className="Slc">
+            <h3>Category:</h3>
+            <select
+              value={category}
+              onChange={(e) => {
+                setCategory(e.target.value);
+                newData();
+              }}>
+              <option value="">ALL</option>
+              <option value="">Category 1</option>
+              <option value="">Category 2</option>
+            </select>
+          </div>
           <div className="Slc">
             <h3>Prices:</h3>
             <select
@@ -72,64 +103,44 @@ function Search() {
           </div>
 
           <div className="Slc">
-            <h3>Category:</h3>
-            <select
-              value={category}
-              onChange={(e) => {
-                setCategory(e.target.value);
-                newData();
-              }}>
-              <option value="">ALL</option>
-              <option value="">Category 1</option>
-              <option value="">Category 2</option>
-            </select>
-          </div>
-
-          <div className="Slc">
-           <h3>Max Price:</h3>
-           <h3>{maxPrice}</h3>
-            
+            <h3>Max Price:</h3>
+            <h3>{maxPrice}</h3>
           </div>
           <input
-              type="range"
-              min={100}
-              max={100000}
-              value={maxPrice}
-              onChange={(e) => {
-                setMaxPrice(Number(e.target.value));
-                newData();
-              }}
-            />
-      </div>
+            type="range"
+            min={100}
+            max={100000}
+            value={maxPrice}
+            onChange={(e) => {
+              setMaxPrice(Number(e.target.value));
+              newData();
+            }}
+          />
+        </div>
 
-      <div className="result">
+        <div className="result">
           <h1>Results:</h1>
           <div className="cards">
-            {results.map((item) => (
+            {paginatedItems.map((item) => (
               <Card key={item._id} item={item} />
             ))}
           </div>
-          {results.length === 0 ? <p className="txt">No Results</p> : ""}
+          {paginatedItems.length === 0 ? <p className="txt">No Results</p> : ""}
 
-          {screenWidth > 1000 ? <div className='pgch'>
-      <button
-        disabled={!isPrevPage}
-        onClick={() => setPage((prev) => prev - 1)}
-      >
-        Prev
-      </button>
-      <span>
-        {page} of {4}
-      </span>
-      <button
-        disabled={!isNextPage}
-        onClick={() => setPage((prev) => prev + 1)}
-      >
-        Next
-      </button>
-    </div> : ""}
-      </div>
-
+          <div className="pgch">
+            <button
+              disabled={!isPrevPage}
+              onClick={() => setPage((prev) => prev - 1)}>
+              Prev
+            </button>
+            <span>{page}</span>
+            <button
+              disabled={!isNextPage}
+              onClick={() => setPage((prev) => prev + 1)}>
+              Next
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
